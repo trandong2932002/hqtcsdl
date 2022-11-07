bb4232d4543db35d7b03a4b1c6c96de0ca155da1
HMPhuoc <huynhminhphuoc01@gmail.com>
2022-11-07 12:45:38 +0700

    sp(add,update,del)+transaction+Login+user

--- QuanLyLuongNV.sql
+++ QuanLyLuongNV.sql
@@ -6,28 +6,28 @@ use QUANLYLUONGNV
 go
 create table CHINHANH(
 	MaCN char(5)primary key,
-	DiaChi nvarchar(50),
+	DiaChi nvarchar(100),
 	SoDT char(10) constraint dt_vanphong check (SoDT like'[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' and len(SoDT)=10)
 )
 
 go 
 create table PHONGBAN(
 	MaPB char(5) primary key,
-	MaCN char(5) references CHINHANH(MaCN),
-	TenPB nvarchar(20)
+	MaCN char(5) references CHINHANH(MaCN)on delete set null on update cascade,
+	TenPB nvarchar(50)
 )
 
 go
 create table CONGVIEC(
 	MaCV char(5) primary key,
-	TenCV nvarchar(20),
-	MoTaCV nvarchar(50)
+	TenCV nvarchar(50),
+	MoTaCV nvarchar(200)
 )
 
 create table NHANVIEN(--Khoi tao cac bang lien quan bang trigger
 	MaNV char(10) primary key,
-	MaPB char(5) references PHONGBAN(MaPB),
-	MaCV char(5) references CONGVIEC(MaCV),
+	MaPB char(5) references PHONGBAN(MaPB) on delete set null on update cascade,
+	MaCV char(5) references CONGVIEC(MaCV) on delete set null on update cascade,
 	TenNV nvarchar(50),
 	GioiTinh nvarchar(5) constraint gioitinh check(GioiTinh in(N'Nam',N'Nữ')),
 	SoDT char(10) constraint dt_nhanvien check (SoDT like'[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' and len(SoDT)=10),
@@ -38,19 +38,17 @@ create table NHANVIEN(--Khoi tao cac bang lien quan bang trigger
 
 go
 create table CHAMCONG(
-	MaNV char(10) references NHANVIEN(MaNV),
+	MaNV char(10) references NHANVIEN(MaNV)on update cascade,
 	ThoiGianLamDonVi int,
 	TangCa int, 
 	NgayChamCong date,
 	primary key(MaNV,NgayChamCong)
 )
 
-go
-
 
 go
 create table TINHLUONG(
-	MaNV char(10) references NHANVIEN(MaNV),
+	MaNV char(10) references NHANVIEN(MaNV)on update cascade,
 	TongLuong int,
 	ThoiGianThang date,
 	primary key(MaNV,ThoiGianThang)
@@ -136,12 +134,13 @@ begin
 
 
 --Thong ke
+/*
 go
 select NHANVIEN.MaNV,NHANVIEN.TienTheoDonVi,LoaiLaoDong,ThoiGianLamDonVi,TangCa from
 NHANVIEN join
 (select MaNV,sum(ThoiGianLamDonVi)as ThoiGianLamDonVi,sum(TangCa)as TangCa from CHAMCONG  where MONTH(NgayChamCong)=5 and YEAR(NgayChamCong)=2022group by(MaNV) )as T1
 on NHANVIEN.MaNV=T1.MaNV
-
+*/
 go
 create function f_ThongKe(@ngay date)
 returns table
@@ -157,53 +156,305 @@ on NHANVIEN.MaNV=T1.MaNV)
 
 ---Nhap
 go
-create proc sp_addChiNhanh @MaCN char(5), @DiaChi nvarchar(50),@SoDT char(10)
+create proc sp_addChiNhanh @MaCN char(5), @DiaChi nvarchar(100),@SoDT char(10)
 as
-INSERT into CHINHANH values(@MaCN,@DiaChi,@SoDT)
+begin tran
+	if(@DiaChi is null or @DiaChi = ' ')
+	begin
+		raiserror('Dia chi khong the bo trong',16,1)
+		rollback
+		return
+	end
+	INSERT into CHINHANH values(@MaCN,@DiaChi,@SoDT)
+	if(@@ERROR<>0)
+	begin
+		raiserror('Error',16,1)
+		rollback
+		return
+	end
+commit tran
 
 go
-create proc sp_addPhongBan @MaPB char(5),@MaCN char(5),@TenPB nvarchar(20)
+create proc sp_addPhongBan @MaPB char(5),@MaCN char(5),@TenPB nvarchar(50)
 as
-INSERT into PHONGBAN values(@MaPB,@MaCN,@TenPB)
+begin tran
+	if(@MaCN is null or @MaCN = ' ')
+	begin
+		raiserror('De nghi nhap ma chi nhanh',16,1)
+		rollback
+		return
+	end
+	if(@TenPB is null or @TenPB = ' ')
+	begin
+		raiserror('De nghi nhap ten phong ban',16,1)
+		rollback
+		return
+	end
+	INSERT into PHONGBAN values(@MaPB,@MaCN,@TenPB)
+	if(@@ERROR<>0)
+	begin
+		raiserror('Error',16,1)
+		rollback
+		return
+	end
+commit tran
+	
 
 go
-create proc sp_addCongViec @MaCV char(5),@TenCV nvarchar(20),@MoTaCV nvarchar(50)
+create proc sp_addCongViec @MaCV char(5),@TenCV nvarchar(50),@MoTaCV nvarchar(200)
 as
-INSERT into CONGVIEC values(@MaCV,@TenCV,@MoTaCV)
+begin tran
+	if(@TenCV is null or @TenCV = ' ')
+	begin
+		raiserror('Khong de trong Ten cong viec',16,1)
+		rollback
+		return
+	end
+	if(@MoTaCV is null or @MoTaCV = ' ')
+	begin
+		raiserror('Hay nhap mo ta cong viec',16,1)
+		rollback
+		return
+	end
+	INSERT into CONGVIEC values(@MaCV,@TenCV,@MoTaCV)
+	if(@@ERROR<>0)
+	begin
+		raiserror('Error',16,1)
+		rollback
+		return
+	end
+commit tran
 
 go
 create proc sp_addNhanVien @MaNV nvarchar(10),@MaPB nvarchar(5),@MaCV nvarchar(5),@TenNV nvarchar(50),@GioiTinh nvarchar(5),@SoDT char(10),@LoaiLaoDong nvarchar(20),@LuongDonVi int
 as
-INSERT into NHANVIEN values(@MaNV,@MaPB,@MaCV,@TenNV,@GioiTinh,@SoDT,@LoaiLaoDong,@LuongDonVi)
+begin tran
+	if(@MaPB is null or @MaPB = ' ')
+	begin
+		raiserror('Hay chon phong ban',16,1)
+		rollback
+		return
+	end
+	if(@MaCV is null or @MaCV = ' ')
+	begin
+		raiserror('Hay chon cong viec',16,1)
+		rollback
+		return
+	end
+	if(@TenNV is null or @TenNV = ' ')
+	begin
+		raiserror('Hay nhap ten',16,1)
+		rollback
+		return
+	end
+	if(@LuongDonVi is null)
+	begin
+		raiserror('Hay nhap luong',16,1)
+		rollback
+		return
+	end
+	INSERT into NHANVIEN values(@MaNV,@MaPB,@MaCV,@TenNV,@GioiTinh,@SoDT,@LoaiLaoDong,@LuongDonVi)
+	if(@@ERROR<>0)
+	begin
+		raiserror('Error',16,1)
+		rollback
+		return
+	end
+commit tran
 
 go
 create proc sp_ChamCong @MaNV char(10), @ThoiGian int, @TangCa int
 as
-INSERT into CHAMCONG values(@MaNV,@ThoiGian,@TangCa,GETDATE())
+begin tran
+	if(@ThoiGian is null or @ThoiGian < 0 or @ThoiGian>16)
+	begin
+		raiserror('De nghi nhap thoi gian chinh xac',16,1)
+		rollback
+		return
+	end
+	if(@TangCa is null or @TangCa<0 or @TangCa>8)
+	begin
+		raiserror('De nghi nhap thoi gian tang ca chinh xac',16,1)
+		rollback
+		return
+	end
+	INSERT into CHAMCONG values(@MaNV,@ThoiGian,@TangCa,GETDATE())
+	if(@@ERROR<>0)
+	begin
+		raiserror('Error',16,1)
+		rollback
+		return
+	end
+commit tran
 
 
 ---Sua
 go
 create proc sp_updateChiNhanh @MaCN char(5), @DiaChi nvarchar(50),@SoDT char(10)
 as
-UPDATE CHINHANH set DiaChi=@DiaChi,SoDT=@SoDT where MaCN=@MaCN
+begin tran
+	if(@DiaChi is null or @DiaChi = ' ')
+	begin
+		raiserror('Dia chi khong the bo trong',16,1)
+		rollback
+		return
+	end
+	UPDATE CHINHANH set DiaChi=@DiaChi,SoDT=@SoDT where MaCN=@MaCN
+	if(@@ERROR<>0)
+	begin
+		raiserror('Error',16,1)
+		rollback
+		return
+	end
+commit tran
 
 go
 create proc sp_updatePhongBan @MaPB char(5),@MaCN char(5),@TenPB nvarchar(20)
 as
-UPDATE PHONGBAN set TenPB=@TenPB,MaCN=@MaCN where MaPB=@MaPB
+begin tran
+	if(@MaCN is null or @MaCN = ' ')
+	begin
+		raiserror('De nghi nhap ma chi nhanh',16,1)
+		rollback
+		return
+	end
+	if(@TenPB is null or @TenPB = ' ')
+	begin
+		raiserror('De nghi nhap ten phong ban',16,1)
+		rollback
+		return
+	end
+	UPDATE PHONGBAN set TenPB=@TenPB,MaCN=@MaCN where MaPB=@MaPB
+	if(@@ERROR<>0)
+	begin
+		raiserror('Error',16,1)
+		rollback
+		return
+	end
+commit tran
 
 go
 create proc sp_updateCongViec @MaCV char(5),@TenCV nvarchar(20),@MoTaCV nvarchar(50)
 as
-UPDATE CONGVIEC set TenCV=@TenCV,MoTaCV=@MoTaCV where MaCV=@MaCV
+begin tran
+	if(@TenCV is null or @TenCV = ' ')
+	begin
+		raiserror('Khong de trong Ten cong viec',16,1)
+		rollback
+		return
+	end
+	if(@MoTaCV is null or @MoTaCV = ' ')
+	begin
+		raiserror('Hay nhap mo ta cong viec',16,1)
+		rollback
+		return
+	end
+	UPDATE CONGVIEC set TenCV=@TenCV,MoTaCV=@MoTaCV where MaCV=@MaCV
+	if(@@ERROR<>0)
+	begin
+		raiserror('Error',16,1)
+		rollback
+		return
+	end
+commit tran
 
 go
 create proc sp_updateNhanVien @MaNV nvarchar(10),@MaPB nvarchar(5),@MaCV nvarchar(5),@TenNV nvarchar(50),@GioiTinh nvarchar(5),@SoDT char(10),@LoaiLaoDong nvarchar(20),@LuongDonVi int
 as
-UPDATE NHANVIEN set MaCV=@MaCV,MaPB=@MaPB,TenNV=@TenNV,GioiTinh=@GioiTinh,SoDT=@SoDT,LoaiLaoDong=@LoaiLaoDong,TienTheoDonVi=@LuongDonVi where MaNV=@MaNV
+begin tran
+	if(@MaPB is null or @MaPB = ' ')
+	begin
+		raiserror('Hay chon phong ban',16,1)
+		rollback
+		return
+	end
+	if(@MaCV is null or @MaCV = ' ')
+	begin
+		raiserror('Hay chon cong viec',16,1)
+		rollback
+		return
+	end
+	if(@TenNV is null or @TenNV = ' ')
+	begin
+		raiserror('Hay nhap ten',16,1)
+		rollback
+		return
+	end
+	if(@LuongDonVi is null)
+	begin
+		raiserror('Hay nhap luong',16,1)
+		rollback
+		return
+	end
+	UPDATE NHANVIEN set MaCV=@MaCV,MaPB=@MaPB,TenNV=@TenNV,GioiTinh=@GioiTinh,SoDT=@SoDT,LoaiLaoDong=@LoaiLaoDong,TienTheoDonVi=@LuongDonVi where MaNV=@MaNV
+	if(@@ERROR<>0)
+	begin
+		raiserror('Error',16,1)
+		rollback
+		return
+	end
+commit tran
 
+--Xoa
+go
+create proc sp_delChiNhanh @MaCN char(5)
+as
+begin tran
+	DELETE from CHINHANH where MaCN = @MaCN 
+	if(@@ERROR<>0)
+	begin
+		raiserror('Error',16,1)
+		rollback
+		return
+	end
+commit tran
+
+go 
+create proc sp_delPhongBan @MaPB char(5)
+as
+begin tran
+	DELETE from PHONGBAN where MaPB = @MaPB
+	if(@@ERROR<>0)
+	begin
+		raiserror('Error',16,1)
+		rollback
+		return
+	end
+commit tran
+
+go
+create proc sp_delCongViec @MaCV char(5)
+as
+begin tran
+	DELETE from CONGVIEC where MaCV = @MaCV
+	if(@@ERROR<>0)
+	begin
+		raiserror('Error',16,1)
+		rollback
+		return
+	end
+commit tran
+
+go
+create proc sp_delNhanVien @MaNV char(5)
+as 
+begin tran
+	DELETE from NHANVIEN where MaNV=@MaNV
+	if(@@ERROR<>0)
+	begin
+		raiserror('Error',16,1)
+		rollback
+		return
+	end
+commit tran
 --Tinh Luong Thang
+
+go
+create function f_TinhLuong(@NgayTinhLuong date)
+returns table
+as
+return
+(
 select NHANVIEN.MaNV,NHANVIEN.TienTheoDonVi,LoaiLaoDong,ThoiGianLamDonVi,TangCa,
 TienLuong = case LoaiLaoDong
 when 'ToanThoiGian' then TienTheoDonVi * (ThoiGianLamDonVi * 1 + TangCa * 1.5)
@@ -211,5 +462,25 @@ when 'BanThoiGian' then TienTheoDonVi * 0.75 * (ThoiGianLamDonVi * 1 + TangCa * 
 end
 from
 NHANVIEN join
-(select MaNV,sum(ThoiGianLamDonVi)as ThoiGianLamDonVi,sum(TangCa)as TangCa from CHAMCONG  where MONTH(NgayChamCong)=1 and YEAR(NgayChamCong)=2000 group by(MaNV) )as T1
+(select MaNV,sum(ThoiGianLamDonVi)as ThoiGianLamDonVi,sum(TangCa)as TangCa from CHAMCONG  where MONTH(NgayChamCong)=MONTH(@NgayTinhLuong) and YEAR(NgayChamCong)=YEAR(@NgayTinhLuong) group by(MaNV) )as T1
 on NHANVIEN.MaNV=T1.MaNV
+)
+
+--Tao User 
+--Admin:admin
+go
+create login Admin with password = 'admin'
+go
+create user Admin for login Admin
+
+--Manager:manager
+go
+create login Manager with password = 'manager'
+go
+create user Manager for login Manager
+
+/*
+select *from f_TinhLuong('2022-2-2')
+select *from f_ThongKe('2022-2-2')
+sp(add,update,del)+transaction+Login+user
+*/
